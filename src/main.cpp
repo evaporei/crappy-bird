be7005be65f686a43a251a9b0bea78b594df62f4
Eva Pace <eba.pachi@gmail.com>
2024-09-23 13:56:47 -0300

    draw background & ground

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <raylib.h>
 
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+#define GAME_WIDTH 512
+#define GAME_HEIGHT 288
+
 int main(void) {
 #ifndef DEBUG
     SetTraceLogLevel(LOG_ERROR);
 #endif
 
-    InitWindow(800, 450, "flappy bird");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "flappy bird");
+
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){ 0, 0 };
+    camera.offset = (Vector2){ 0, 0 };
+    camera.rotation = 0.0f;
+    camera.zoom = 2.5f;
+
+    Texture2D background = LoadTexture("background.png");
+    Texture2D ground = LoadTexture("ground.png");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("game content here.", 190, 200, 20, LIGHTGRAY);
+            BeginMode2D(camera);
+                DrawTexture(background, 0, 0, WHITE);
+                DrawTexture(ground, 0, GAME_HEIGHT - 16, WHITE);
+            EndMode2D();
+            DrawFPS(0, 0);
         EndDrawing();
     }
+
+    UnloadTexture(ground);
+    UnloadTexture(background);
 
     CloseWindow();
 
